1cccdbabdde45861dea8114637ba11295268edf7
Nate Maile <npmaile@gmail.com>
2021-05-17 21:06:32 -0400

    better setup script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
-set -e
+function need(){
+	local __resultvar=$1
+	if command $1 --version &> /dev/null
+	then
+		eval $__resultvar='0'
+	else
+		eval $__resultvar='1'
+	fi
+}
+
+need nvim
+if [[ $nvim == 0 ]]; then
 ########
 #neovim#
 ########
+echo "starting deploy of vim config"
 #vim-go
 git submodule sync neovim/.local/share/nvim/site/pack/go/start/vim-go
 git submodule set-branch --branch v1.25 neovim/.local/share/nvim/site/pack/go/start/vim-go
@@ -11,9 +23,18 @@ git submodule sync neovim/.local/share/nvim/site/pack/gnupg/start/vim-gnupg
 git submodule set-branch --branch master neovim/.local/share/nvim/site/pack/gnupg/start/vim-gnupg
 #stow it
 stow neovim -t $HOME
+else
+	echo "skipping neovim because nvim cannot be found"
+fi
 
+need alacritty
+if [[ $alacritty == 0 ]]; then
 ###########
 #alacritty#
 ###########
+echo "starting deploy of alacritty conifg"
 #stow it
 stow alacritty -t $HOME
+else
+	echo "skipping alacritty because alacritty cannot be found"
+fi
