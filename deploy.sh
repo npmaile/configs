1c40b1cada614548c3796cf569cbd6924e756e0a
Nate Maile <npmaile@gmail.com>
2021-05-17 20:47:22 -0400

    alacritty config

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 set -e
-git submodule foreach git pull --recursive
+########
+#neovim#
+########
+#vim-go
+git submodule sync neovim/.local/share/nvim/site/pack/go/start/vim-go
+git submodule set-branch --branch v1.25 neovim/.local/share/nvim/site/pack/go/start/vim-go
+#vim-gnupg
+git submodule sync neovim/.local/share/nvim/site/pack/gnupg/start/vim-gnupg
+git submodule set-branch --branch master neovim/.local/share/nvim/site/pack/gnupg/start/vim-gnupg
+#stow it
 stow neovim -t $HOME
+
+###########
+#alacritty#
+###########
+#stow it
+stow alacritty -t $HOME
