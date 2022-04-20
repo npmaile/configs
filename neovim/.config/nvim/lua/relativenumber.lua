944b892f3738939a7c1d1daf49697bcb760a6cb2
Nathaniel Maile <npmaile@gmail.com>
2022-04-19 22:53:29 -0400

    luatize relative number

--- neovim/.config/nvim/lua/relativenumber.lua
+++ neovim/.config/nvim/lua/relativenumber.lua
@@ -1,11 +1,9 @@
 --relative line numbering
-vim.cmd [[
-set number
-set relativenumber
+vim.api.nvim_set_option_value("number",true,{})
+vim.api.nvim_set_option_value("relativenumber",true,{})
 
-augroup numbertoggle
-autocmd!
-autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
-autocmd BufLeave,FocusLost,InsertEnter * set norelativenumber
-augroup END
-]]
+vim.api.nvim_create_augroup("numbertoggle",{})
+
+vim.api.nvim_create_autocmd({"BufEnter","FocusGained","InsertLeave"},{pattern = {"*"},callback = function() vim.api.nvim_set_option_value("relativenumber", true, {}) end,group = "numbertoggle"})
+
+vim.api.nvim_create_autocmd({"BufLeave","FocusLost","insertEnter"},{pattern = {"*"},callback = function() vim.api.nvim_set_option_value("relativenumber", false, {}) end,group = "numbertoggle"})
