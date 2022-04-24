8dde58720cb82fc6c88f2fc8cef91d683c8cf79a
Nate Maile <npmaile@gmail.com>
2022-04-23 20:56:08 -0400

    things

--- neovim/.config/nvim/lua/relativenumber.lua
+++ neovim/.config/nvim/lua/relativenumber.lua
@@ -4,5 +4,5 @@ vim.api.nvim_set_option_value("relativenumber",true,{})
 
 vim.api.nvim_create_augroup("numbertoggle",{})
 
-vim.api.nvim_create_autocmd({"BufEnter","FocusGained","InsertLeave"},{pattern = {"*"},callback = function() vim.api.nvim_set_option_value("relativenumber", true, {}) end,group = "numbertoggle"})
-vim.api.nvim_create_autocmd({"BufLeave","FocusLost","insertEnter"},{pattern = {"*"},callback = function() vim.api.nvim_set_option_value("relativenumber", false, {}) end,group = "numbertoggle"})
+vim.api.nvim_create_autocmd({"BufEnter","FocusGained","InsertLeave"},{pattern = "*",callback = function() vim.api.nvim_set_option_value("relativenumber", true, {}) end,group = "numbertoggle"})
+vim.api.nvim_create_autocmd({"BufLeave","FocusLost","insertEnter"},{pattern = "*",callback = function() vim.api.nvim_set_option_value("relativenumber", false, {}) end,group = "numbertoggle"})
