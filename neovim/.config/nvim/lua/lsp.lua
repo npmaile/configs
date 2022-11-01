a3295496dd9f38dd31b34cb6be7c5d9ab01d57ea
Nate Maile <npmaile@gmail.com>
2022-11-01 00:59:34 -0400

    fix deprication warning

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -54,7 +54,7 @@ local on_attach = function(client, bufnr)
 	})
 end
 --capabilities
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 
