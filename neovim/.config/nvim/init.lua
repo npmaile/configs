40042f91be364d673f8955cec2fcb878697d3324
Nate Maile <npmaile@gmail.com>
2026-05-08 17:06:14 -0400

    fix treesitter setup

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -5,7 +5,16 @@ require('lsp')
 require('nvim-cmp')
 require('relativenumber')
 require('noswap')
-require('treesitter')
 require('statusline')
 require('netrw')
 require('colors')
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  callback = function()
+    local ok = pcall(vim.treesitter.start)
+    if not ok then
+      -- Parser not available for this filetype
+    end
+  end,
+})
--- neovim/.config/nvim/lua/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require'nvim-treesitter.config'.setup {
-  ensure_installed = { "rust", "go" },
-  highlight = {
-    enable = true,
-  },
-}
